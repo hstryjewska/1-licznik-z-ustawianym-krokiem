40e20951cbd273e0db1c4b447b5af149efcc8e48
hstryjewska <hannaolgastryjewska@gmail.com>
2021-09-19 17:22:35 +0200

    upload

---
+++ src/Counter.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import './Counter.css';
+
+import Display from './Display';
+import ButtonsPanel from './ButtonsPanel';
+import Step from './Step';
+
+// komponent klasowy
+class Counter extends Component {
+
+    constructor (props) {
+        super(props);
+        this.state = {
+            counterValue: this.props.initValue,
+            stepValue: 5,
+
+        };
+
+    }
+
+    changeStep = (step) => {
+        this.setState({ stepValue: parseFloat(step) })
+    }
+
+    changeValue = (action) => {
+
+        this.setState((prevState, prevProps) => {
+            let currentCounterValue = prevState.counterValue;
+            if (action === 'add') {
+                currentCounterValue += this.state.stepValue;
+            } else if (action === 'reinit') {
+                currentCounterValue = prevProps.initValue;
+            } else {
+                currentCounterValue = 0;
+            }
+            return ({
+                counterValue: currentCounterValue
+            });
+        });
+    }
+
+    render() {
+        return (
+            <div className="counter">
+                Licznik:
+                <Display displayValue={ this.state.counterValue } />
+                <ButtonsPanel buttonMethod={ this.changeValue } />
+                <Step stepValue={ this.state.stepValue } updateStep={ this.changeStep } />
+            </div>
+        );
+    }
+}
+
+export default Counter;
